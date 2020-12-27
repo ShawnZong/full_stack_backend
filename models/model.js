4250ed1025de4beec55877a5713160cae7408d91
shawnjstan <29206292+ShawnZong@users.noreply.github.com>
2020-12-27 22:53:14 +0200

    add3.19 database schema validation and handle validation errors

--- models/model.js
+++ models/model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
 const url = process.env.MONGODB_URL;
 console.log("connecting to", url);
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -9,9 +11,18 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     console.log("error connecting to MongoDB:", error.message);
   });
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  number: {
+    type: String,
+    required: true,
+  },
 });
+personSchema.plugin(uniqueValidator);
+
 personSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
